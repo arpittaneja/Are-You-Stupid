64b37e1fe232fdc8802aa09f88ee63ba08874468
Arpit <75125324+arpittaneja@users.noreply.github.com>
2022-12-16 16:20:00 +0530

    Clean code

--- script.js
+++ script.js
@@ -3,14 +3,10 @@ const yesButton = document.querySelector("#yes");
 const noButton = document.querySelector("#no");
 
 no.addEventListener("click", (e) => {
-    console.log("screen", window.innerHeight, window.innerWidth)
     let newTop = Math.floor(Math.random() * (window.innerHeight - 100));
     let newLeft = Math.floor(Math.random() * (window.innerWidth - 200));
     body.style.position = "relative";
     e.target.style.position = "absolute";
     e.target.style.top = `${newTop}px`;
     e.target.style.left = `${newLeft}px`;
-    if (e.target.offsetTop < 0) {
-        e.target.style.top = -e.target.style.top;
-    }
 })
